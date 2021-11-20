7f04e967d02ec3fb5ecfe233926c2135492b9025
ShishaK1N9 <st077017@student.spbu.ru>
2021-11-20 19:09:21 +0300

    Changed `build.gradle` to `build.gradle.kts`

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.4.21"
+    id("jacoco")
+}
+
+group = "me.user"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation(platform("org.junit:junit-bom:5.7.1"))
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.21")
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+    useJUnitPlatform()
+    maxHeapSize = "2G"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
+}
