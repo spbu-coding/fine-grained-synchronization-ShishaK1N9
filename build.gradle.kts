e4996757c95e42b1b8d4d4c99437c6443a758c78
ShishaK1N9 <st077017@student.spbu.ru>
2021-11-20 20:14:19 +0300

    Add creating .csv file report

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ tasks.jacocoTestReport {
     dependsOn(tasks.test) // tests are required to run before generating the report
     reports {
         xml.required.set(false)
-        csv.required.set(false)
-        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+        csv.outputLocation.set(file("${buildDir}/jacoco/report.csv"))
+        html.outputLocation.set(file("${buildDir}/reports/jacoco"))
     }
 }
