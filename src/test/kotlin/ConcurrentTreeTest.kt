07a7fac1aaab469d8bc5a0a5640904301f6da6bf
ShishaK1N9 <st077017@student.spbu.ru>
2021-11-24 02:03:11 +0300

    Made concurrent tree test

--- src/test/kotlin/ConcurrentTreeTest.kt
+++ src/test/kotlin/ConcurrentTreeTest.kt
@@ -1,18 +1,44 @@
 import concurrent_tree.ConcurrentTree
+import utils.ITree
 
 import org.jetbrains.kotlinx.lincheck.annotations.Operation
 import org.jetbrains.kotlinx.lincheck.annotations.Param
 import org.jetbrains.kotlinx.lincheck.check
 import org.jetbrains.kotlinx.lincheck.paramgen.IntGen
+import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.ModelCheckingOptions
 import org.jetbrains.kotlinx.lincheck.strategy.stress.StressOptions
 import org.jetbrains.kotlinx.lincheck.verifier.VerifierState
 import org.junit.jupiter.api.Test
+import java.util.concurrent.ConcurrentHashMap
 
 @Param.Params(
     Param(name = "key", gen = IntGen::class, conf = "-10:10"),
     Param(name = "value", gen = IntGen::class, conf = "-10:10")
 )
 class ConcurrentTreeTest : VerifierState() {
+
+    class SimpleTree : ITree<Int, Int>, VerifierState() {
+
+        private val map = ConcurrentHashMap<Int, Int>()
+
+        @Operation
+        override fun search(key: Int) =
+            if (map.containsKey(key)) map[key]
+            else null
+
+        @Operation
+        override fun remove(key: Int) = map.remove(key) != null
+
+        @Operation
+        override fun insert(key: Int, value: Int) =
+            if (map.containsKey(key)) false
+            else {
+                map[key] = value
+                true
+            }
+
+        override fun extractState() = map
+    }
 
     private val tree = ConcurrentTree<Int, Int>()
 
@@ -41,16 +67,24 @@ class ConcurrentTreeTest : VerifierState() {
     @Test
     fun stressTestOn5Threads() = StressOptions()
         .actorsBefore(1)
-        .threads(5).actorsPerThread(3)
+        .threads(5).actorsPerThread(5)
         .minimizeFailedScenario(false)
         .check(this::class.java)
 
     /**
-     * Stress test, which runs on 500 iterations.
+     * Stress test, which runs 500 scenarios.
      */
     @Test
-    fun stressTestWith500Iterations() = StressOptions()
+    fun stressTestWith500Scenarios() = StressOptions()
         .iterations(500)
+        .check(this::class.java)
+
+    /**
+     * Compares concurrent tree implementation with concurrent hash map implementation.
+     */
+    @Test
+    fun modelCheckingTest() = ModelCheckingOptions()
+        .sequentialSpecification(SimpleTree::class.java)
         .check(this::class.java)
 
     override fun extractState(): Any {
